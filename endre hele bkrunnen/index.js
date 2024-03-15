168e5c832a93500dd357961869b329d081fae8f7
adriankoder <adrian.koder@gmail.com>
2024-03-16 00:16:38 +0100

    kalkulator

--- endre hele bkrunnen/index.js
+++ endre hele bkrunnen/index.js
@@ -1,37 +1,19 @@
-//let hungry = true;
-//const ishungry = "personen er sulten" + hungry;
+//link til html
+const farge = document.querySelector(".color");
+let darkmode = true;
 
-//console.log(ishungry)
-//const tekst1 = 5
-//const tekst2 = 10
-//console.log(tekst1 + tekst2)
-
-
-
-
-// const tekst1 = 5
-// const tekst2 = 10
-// function addnums() {
-
-// const sum =tekst1 + tekst2;
-// console.log(sum)}
-// addnums ();
-// function changeColor() {
-//     // let colorBox = document.getElementById("colorBox");
-// }
-
-// let knapp = document.getElementById("minKnapp");
-
-// Legg til en lytter for museklikk på knappen
-
-const farge = document.querySelector("#click_me");
-let darkmode = false;
+// Eventlistener for knappen
 farge.addEventListener("click", function() {
     // Koden her blir utført når knappen blir klikket
-  
- farge.style.backgroundColor = "black";
-});changeColor
-function changeColor(){
-    farge.style.backgroundColor = "black";
-    console.log = (darkmode);
+    changeColor();
+});
+
+function changeColor() {
+    darkmode = !darkmode;
+    console.log(darkmode);
+    if (darkmode) {
+        farge.classList.add('black');
+    } else {
+        farge.classList.remove('color');
+    }
 }
